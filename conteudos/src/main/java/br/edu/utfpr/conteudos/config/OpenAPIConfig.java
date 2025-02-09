b853afaa681c241b7d6f46adefedd0b58148859b
ArnoldReis <arnold.reis@outlook.com.br>
2025-02-09 20:52:11 -0300

    Feat: Ajuste na documentação Swagger

--- conteudos/src/main/java/br/edu/utfpr/conteudos/config/OpenAPIConfig.java
+++ conteudos/src/main/java/br/edu/utfpr/conteudos/config/OpenAPIConfig.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 @Configuration
 @OpenAPIDefinition(
     info = @Info(
-        title = "API de Categorias",
+        title = "API de Conteudos",
         version = "1.0",
         description = "API para gerenciamento de categorias, incluindo operações de criação, leitura, atualização e exclusão."
     )
